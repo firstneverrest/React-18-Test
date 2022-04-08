8078a1479379ae5a70f9b4021eed4f2dec49979f
Chitsanupong <first30952@gmail.com>
2022-04-08 14:27:19 +0700

    feat: suspense test

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, SuspenseList, Suspense } from 'react';
+import './App.css';
+
+import User from './components/User';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Suspense fallback={<div>Loading Users1...</div>}>
+      <User id="1" />
+      <Suspense fallback={<div>Loading Users2...</div>}>
+        <User id="2" />
+        <Suspense fallback={<div>Loading Users3...</div>}>
+          <User id="3" />
+        </Suspense>
+      </Suspense>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const User = ({ id }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+
+  return (
+    <div>
+      <h2>All Users {id}</h2>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default User;
